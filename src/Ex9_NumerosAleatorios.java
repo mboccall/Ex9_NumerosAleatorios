b8fd05806305be7bce685f92d948c3e0e562111d
mboccall <mboccalletti@gmail.com>
2023-02-03 16:23:21 -0300

    first commit

---
+++ src/Ex9_NumerosAleatorios.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class Ex9_NumerosAleatorios {
+    public static void main(String[] args) {
+        
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+        
+        for(int i = 0; i < numerosAleatorios.length; i++){
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+        
+        System.out.print("Numeros Aleatorios: ");
+        for(int numero : numerosAleatorios){
+            System.out.print(numero + " ");
+        }
+        
+        System.out.print("\nSucessores dos numeros aleatorios: ");
+        for (int numero : numerosAleatorios){
+            System.out.print((numero+1) + " ");
+        }
+
+        System.out.print("\nAntecessores dos numeros aleatorios: ");
+        for (int numero : numerosAleatorios){
+            System.out.print((numero-1) + " ");
+        }
+    }
+}
